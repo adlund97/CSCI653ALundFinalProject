936258f9b55b550c21b1784c0298de35f1f3f765
Alex Lund <alexmvp21@gmail.com>
2023-05-06 22:33:22 -0500

    testing

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const cors = require('cors')
 const connectDB = require('./config/db')
 const port = process.env.PORT || 5000
 
-app.use(cors())
-
 connectDB()
 
 const app = express()
+
+app.use(cors())
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
